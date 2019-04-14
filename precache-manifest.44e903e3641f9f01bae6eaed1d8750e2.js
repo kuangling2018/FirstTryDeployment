a311b01bad5e08d28b5f3e44eebe837103dea2c6
kuangling <472212029@qq.com>
2019-04-14 22:29:17 +0800

    第一次尝试git部署

---
+++ precache-manifest.44e903e3641f9f01bae6eaed1d8750e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "397b33fccc540204a0b0",
+    "url": "/static/js/main.4d845648.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "397b33fccc540204a0b0",
+    "url": "/static/css/main.be9cd952.chunk.css"
+  },
+  {
+    "revision": "5f579811e9a394c66f265f2c021cc054",
+    "url": "/index.html"
+  }
+];
